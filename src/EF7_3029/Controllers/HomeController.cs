7af001648bd533f8bef74473619e808439ec016a
Dave Zych <david.zych@dptechnology.com>
2015-10-07 15:33:57 -0700

    Added sample database call

--- src/EF7_3029/Controllers/HomeController.cs
+++ src/EF7_3029/Controllers/HomeController.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EF7_3029.Data;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
 
 namespace EF7_3029.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MyContext _context;
+
+        public HomeController(MyContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var things = _context.Forums.Include(f => f.Topics).Select(f => new
+            {
+                f.Id,
+                Count = f.Topics.Count
+            });
+
             return View();
         }
     }
