d347df471eb5e4301099e8c18f395db152d65083
Dave Zych <david.zych@dptechnology.com>
2015-10-07 15:21:15 -0700

    Added base entities

--- src/EF7_3029/Data/MyContext.cs
+++ src/EF7_3029/Data/MyContext.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.Infrastructure;
 
@@ -11,6 +13,9 @@ namespace EF7_3029.Data
 
         }
         
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+
         public MyContext(DbContextOptions options) : base(options)
         {
 
@@ -18,6 +23,34 @@ namespace EF7_3029.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Forum>(entity =>
+            {
+                entity.Property(e => e.Title).HasMaxLength(80);
+            });
+
+            modelBuilder.Entity<Topic>(entity =>
+            {
+                entity.Property(e => e.Title).HasMaxLength(80);
+            });
+        }
+
+        public class Forum
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            
+            public ICollection<Topic> Topics { get; set; }
+        }
+
+        public class Topic
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public int ForumId { get; set; }
+
+            [ForeignKey(nameof(ForumId))]
+            [InverseProperty("Topics")]
+            public Forum Forum { get; set; }
         }
     }
 }
