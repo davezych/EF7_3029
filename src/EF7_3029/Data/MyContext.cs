42fc1106a5d7dd4e72445f3b8f3b8216f2172256
Dave Zych <david.zych@dptechnology.com>
2015-10-07 15:13:53 -0700

    Initial setup of context

---
+++ src/EF7_3029/Data/MyContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+
+namespace EF7_3029.Data
+{
+    public partial class MyContext : DbContext
+    {
+        public MyContext()
+        {
+
+        }
+        
+        public MyContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
--- src/EF7_3029/Startup.cs
+++ src/EF7_3029/Startup.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using EF7_3029.Data;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Data.Entity;
 
 namespace EF7_3029
 {
@@ -13,13 +10,25 @@ namespace EF7_3029
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=EF7_3029.AspNet5;Trusted_Connection=True;";
+
+            services.AddEntityFramework()
+                   .AddSqlServer()
+                   .AddDbContext<MyContext>(
+                       options => options.UseSqlServer(connection));
+
+            services.AddMvc();
+
+            services.AddScoped<MyContext>();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
+            app.UseDeveloperExceptionPage();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}");
             });
         }
     }
